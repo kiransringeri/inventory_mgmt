ce78a035d47c0ff25dbcac0c13286f223b3b0025
kiransringeri <kiransringeri@kirans-mbp.dlink>
2018-08-21 15:39:47 +0530

    Storing each change separately, so that we can easily group them.

--- src/main/java/com/weavedin/inventory_mgmt/impl/InventoryManagementImpl.java
+++ src/main/java/com/weavedin/inventory_mgmt/impl/InventoryManagementImpl.java
@@ -2,11 +2,11 @@ package com.weavedin.inventory_mgmt.impl;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
@@ -31,25 +31,25 @@ public class InventoryManagementImpl implements InventoryManagementAPI {
       action.setActionTime(Calendar.getInstance().getTime());
       action.setUserId(userId);
       action.setBranchId(branchId);
-      action.setItemName("item "+item.getName());
-      
+      action.setItemName("item " + item.getName());
+
       DataDAOFactory daoFactry = new DataDAOFactory();
       SessionFactory sessionFactry = FactoryUtil.getSessionFactory();
-      
+
       txn = sessionFactry.getCurrentSession().beginTransaction();
       daoFactry.getItemDAO().save(item);
       daoFactry.getUserActionDAO().save(action);
       txn.commit();
-      
+
       retObj.setReturnData(item);
-      
-    }catch(Throwable th) {
+
+    } catch (Throwable th) {
       retObj.setError(true);
       retObj.setException(th);
       try {
         txn.rollback();
       } catch (Exception e) {
-        
+
       }
     }
     return retObj;
@@ -63,105 +63,108 @@ public class InventoryManagementImpl implements InventoryManagementAPI {
       DataDAOFactory daoFactry = new DataDAOFactory();
       SessionFactory sessionFactry = FactoryUtil.getSessionFactory();
       txn = sessionFactry.getCurrentSession().beginTransaction();
-      
+
       Item itemInDB = daoFactry.getItemDAO().findByPrimaryKey(item.getId());
-      String changes = updateItemData(itemInDB, item);
+      Collection<String> changes = updateItemData(itemInDB, item);
       daoFactry.getItemDAO().update(itemInDB);
 
-      if(changes != null) {
-        UserAction action = new UserAction();
-        action.setAction("Edited");
-        action.setActionTime(Calendar.getInstance().getTime());
-        action.setUserId(userId);
-        action.setBranchId(branchId);
-        action.setItemName("item "+item.getName());
-        action.setProperty(changes);
-        daoFactry.getUserActionDAO().save(action);
+      if (changes != null && !changes.isEmpty()) {
+        Date currTime = Calendar.getInstance().getTime();
+        for (String change : changes) {
+          UserAction action = new UserAction();
+          action.setAction("Edited");
+          action.setActionTime(currTime);
+          action.setUserId(userId);
+          action.setBranchId(branchId);
+          action.setItemName("item " + item.getName());
+          action.setProperty(change);
+          daoFactry.getUserActionDAO().save(action);
+        }
       }
       txn.commit();
       retObj.setReturnData(itemInDB);
-    }catch(Throwable th) {
+    } catch (Throwable th) {
       retObj.setError(true);
       retObj.setException(th);
       try {
         txn.rollback();
       } catch (Exception e) {
-        
+
       }
     }
     return retObj;
   }
 
-  private String updateItemData(Item itemInDB, Item updatedItem) {
+  private Collection<String> updateItemData(Item itemInDB, Item updatedItem) {
     List<String> updatedProps = new ArrayList<>();
-    if(!equals(itemInDB.getBrand(), updatedItem.getBrand())){
+    if (!equals(itemInDB.getBrand(), updatedItem.getBrand())) {
       itemInDB.setBrand(updatedItem.getBrand());
       updatedProps.add("brand");
     }
-    if(!equals(itemInDB.getCategory(), updatedItem.getCategory())){
+    if (!equals(itemInDB.getCategory(), updatedItem.getCategory())) {
       itemInDB.setCategory(updatedItem.getCategory());
       updatedProps.add("category");
     }
-    if(!equals(itemInDB.getName(), updatedItem.getName())){
+    if (!equals(itemInDB.getName(), updatedItem.getName())) {
       itemInDB.setName(updatedItem.getName());
       updatedProps.add("name");
     }
-    if(!equals(itemInDB.getProductCode(), updatedItem.getProductCode())){
+    if (!equals(itemInDB.getProductCode(), updatedItem.getProductCode())) {
       itemInDB.setProductCode(updatedItem.getProductCode());
       updatedProps.add("product code");
     }
-    String str = updatedProps.isEmpty() ? null : updatedProps.stream().collect(Collectors.joining(","));
-    return str;
+    return updatedProps;
   }
-  
-  private String updateVariantData(Variant variantInDB, Variant updatedVariant) {
+
+  private Collection<String> updateVariantData(Variant variantInDB, Variant updatedVariant) {
     List<String> updatedProps = new ArrayList<>();
-    if(!equals(variantInDB.getName(), updatedVariant.getName())){
+    if (!equals(variantInDB.getName(), updatedVariant.getName())) {
       variantInDB.setName(updatedVariant.getName());
       updatedProps.add("name");
     }
-    if(variantInDB.getCostPrice() != updatedVariant.getCostPrice()) {
+    if (variantInDB.getCostPrice() != updatedVariant.getCostPrice()) {
       variantInDB.setCostPrice(updatedVariant.getCostPrice());
       updatedProps.add("cost price");
     }
-    if(variantInDB.getSellingPrice() != updatedVariant.getSellingPrice()) {
+    if (variantInDB.getSellingPrice() != updatedVariant.getSellingPrice()) {
       variantInDB.setSellingPrice(updatedVariant.getSellingPrice());
       updatedProps.add("selling price");
     }
-    if(variantInDB.getQuantity() != updatedVariant.getQuantity()) {
+    if (variantInDB.getQuantity() != updatedVariant.getQuantity()) {
       variantInDB.setQuantity(updatedVariant.getQuantity());
       updatedProps.add("quantity");
     }
-    Map<String, String> otherPropsInDB = variantInDB.getProperties() == null ? new HashMap<>() : variantInDB.getProperties();
-    Map<String, String> otherPropsUpdated = updatedVariant.getProperties() == null ? new HashMap<>() : new HashMap<>(updatedVariant.getProperties());
-    for(String prop : otherPropsInDB.keySet()) {
+    Map<String, String> otherPropsInDB =
+        variantInDB.getProperties() == null ? new HashMap<>() : variantInDB.getProperties();
+    Map<String, String> otherPropsUpdated = updatedVariant.getProperties() == null ? new HashMap<>()
+        : new HashMap<>(updatedVariant.getProperties());
+    for (String prop : otherPropsInDB.keySet()) {
       String valInDB = otherPropsInDB.get(prop);
       String valUpdated = otherPropsUpdated.remove(prop);
-      if(!equals(valInDB, valUpdated)) {
+      if (!equals(valInDB, valUpdated)) {
         variantInDB.addProperty(prop, valUpdated);
         updatedProps.add(prop);
       }
     }
-    for(String prop : otherPropsUpdated.keySet()) {
+    for (String prop : otherPropsUpdated.keySet()) {
       String valUpdated = otherPropsUpdated.remove(prop);
       variantInDB.addProperty(prop, valUpdated);
       updatedProps.add(prop);
     }
-    
-    String str = updatedProps.isEmpty() ? null : updatedProps.stream().collect(Collectors.joining(","));
-    return str;
+
+    return updatedProps;
   }
-  
+
   private boolean equals(Object str1, Object str2) {
-    if(str1 == null && str2 == null) {
+    if (str1 == null && str2 == null) {
       return true;
-    }else if(str1 == null || str2 == null) {
+    } else if (str1 == null || str2 == null) {
       return false;
-    }else {
+    } else {
       return str1.equals(str2);
     }
   }
-  
+
   @Override
   public APIResponse<Boolean> deleteItem(long userId, long branchId, long itemId) {
     APIResponse<Boolean> retObj = new APIResponse<>();
@@ -170,27 +173,27 @@ public class InventoryManagementImpl implements InventoryManagementAPI {
       DataDAOFactory daoFactry = new DataDAOFactory();
       SessionFactory sessionFactry = FactoryUtil.getSessionFactory();
       txn = sessionFactry.getCurrentSession().beginTransaction();
-      
+
       Item item = daoFactry.getItemDAO().findByPrimaryKey(itemId);
       daoFactry.getItemDAO().delete(item);
-      
+
       UserAction action = new UserAction();
       action.setAction("Deleted");
       action.setActionTime(Calendar.getInstance().getTime());
       action.setUserId(userId);
       action.setBranchId(branchId);
-      action.setItemName("item "+item.getName());
+      action.setItemName("item " + item.getName());
       daoFactry.getUserActionDAO().save(action);
 
       txn.commit();
       retObj.setReturnData(true);
-    }catch(Throwable th) {
+    } catch (Throwable th) {
       retObj.setError(true);
       retObj.setException(th);
       try {
         txn.rollback();
       } catch (Exception e) {
-        
+
       }
     }
     return retObj;
@@ -207,27 +210,27 @@ public class InventoryManagementImpl implements InventoryManagementAPI {
       txn = sessionFactry.getCurrentSession().beginTransaction();
 
       String itemName = getItemName(sessionFactry, variant.getItemId());
-      
+
       UserAction action = new UserAction();
       action.setAction("Added");
       action.setActionTime(Calendar.getInstance().getTime());
       action.setUserId(userId);
       action.setBranchId(branchId);
-      action.setItemName("variant "+variant.getName()+" of item "+itemName);
-      
+      action.setItemName("variant " + variant.getName() + " of item " + itemName);
+
       daoFactry.getVariantDAO().save(variant);
       daoFactry.getUserActionDAO().save(action);
       txn.commit();
-      
-      retObj.setReturnData(variant == null ? null : (Variant)variant.clone());
-      
-    }catch(Throwable th) {
+
+      retObj.setReturnData(variant == null ? null : (Variant) variant.clone());
+
+    } catch (Throwable th) {
       retObj.setError(true);
       retObj.setException(th);
       try {
         txn.rollback();
       } catch (Exception e) {
-        
+
       }
     }
     return retObj;
@@ -242,45 +245,49 @@ public class InventoryManagementImpl implements InventoryManagementAPI {
       DataDAOFactory daoFactry = new DataDAOFactory();
       SessionFactory sessionFactry = FactoryUtil.getSessionFactory();
       txn = sessionFactry.getCurrentSession().beginTransaction();
-      
+
       String itemName = getItemName(sessionFactry, variant.getItemId());
-      
+
       Variant variantInDB = daoFactry.getVariantDAO().findByPrimaryKey(variant.getId());
-      
-      String changes = updateVariantData(variantInDB, variant);
+
+      Collection<String> changes = updateVariantData(variantInDB, variant);
       daoFactry.getVariantDAO().update(variantInDB);
 
-      if(changes != null) {
-        UserAction action = new UserAction();
-        action.setAction("Edited");
-        action.setActionTime(Calendar.getInstance().getTime());
-        action.setUserId(userId);
-        action.setBranchId(branchId);
-        action.setItemName("variant "+variant.getName()+" of item "+itemName);
-        action.setProperty(changes);
-        daoFactry.getUserActionDAO().save(action);
+      if (changes != null && !changes.isEmpty()) {
+        Date currTime = Calendar.getInstance().getTime();
+        for (String change : changes) {
+          UserAction action = new UserAction();
+          action.setAction("Edited");
+          action.setActionTime(currTime);
+          action.setUserId(userId);
+          action.setBranchId(branchId);
+          action.setItemName("variant " + variant.getName() + " of item " + itemName);
+          action.setProperty(change);
+          daoFactry.getUserActionDAO().save(action);
+        }
       }
       txn.commit();
-      retObj.setReturnData(variantInDB == null ? null : (Variant)variantInDB.clone());
-    }catch(Throwable th) {
+      retObj.setReturnData(variantInDB == null ? null : (Variant) variantInDB.clone());
+    } catch (Throwable th) {
       retObj.setError(true);
       retObj.setException(th);
       try {
         txn.rollback();
       } catch (Exception e) {
-        
+
       }
     }
     return retObj;
   }
 
   private String getItemName(SessionFactory sessionFactry, long itemId) {
-    Query<String> query = sessionFactry.getCurrentSession().createSQLQuery("select name from item where id=:itemId");
+    Query<String> query =
+        sessionFactry.getCurrentSession().createSQLQuery("select name from item where id=:itemId");
     query.setParameter("itemId", itemId);
     String itemName = query.uniqueResult();
     return itemName;
   }
-  
+
   @Override
   public APIResponse<Boolean> deleteVariant(long userId, long branchId, long variantId) {
     APIResponse<Boolean> retObj = new APIResponse<>();
@@ -289,29 +296,29 @@ public class InventoryManagementImpl implements InventoryManagementAPI {
       DataDAOFactory daoFactry = new DataDAOFactory();
       SessionFactory sessionFactry = FactoryUtil.getSessionFactory();
       txn = sessionFactry.getCurrentSession().beginTransaction();
-      
+
       Variant variant = daoFactry.getVariantDAO().findByPrimaryKey(variantId);
       String itemName = getItemName(sessionFactry, variant.getItemId());
-      
+
       daoFactry.getVariantDAO().delete(variant);
-      
+
       UserAction action = new UserAction();
       action.setAction("Deleted");
       action.setActionTime(Calendar.getInstance().getTime());
       action.setUserId(userId);
       action.setBranchId(branchId);
-      action.setItemName("variant "+variant.getName()+" of item "+itemName);
+      action.setItemName("variant " + variant.getName() + " of item " + itemName);
       daoFactry.getUserActionDAO().save(action);
 
       txn.commit();
       retObj.setReturnData(true);
-    }catch(Throwable th) {
+    } catch (Throwable th) {
       retObj.setError(true);
       retObj.setException(th);
       try {
         txn.rollback();
       } catch (Exception e) {
-        
+
       }
     }
     return retObj;
@@ -322,26 +329,26 @@ public class InventoryManagementImpl implements InventoryManagementAPI {
     APIResponse<List<UserActionFeed>> retObj = new APIResponse<>();
     Transaction txn = null;
     try {
-      if(from == null || till == null) {
+      if (from == null || till == null) {
         return null;
       }
-      
+
       DataDAOFactory daoFactry = new DataDAOFactory();
       SessionFactory sessionFactry = FactoryUtil.getSessionFactory();
-      
+
       txn = sessionFactry.getCurrentSession().beginTransaction();
       List<UserActionFeed> items = daoFactry.getUserActionDAO().find(from, till, userId);
       txn.rollback();
-      
+
       retObj.setReturnData(items);
-      
-    }catch(Throwable th) {
+
+    } catch (Throwable th) {
       retObj.setError(true);
       retObj.setException(th);
       try {
         txn.rollback();
       } catch (Exception e) {
-        
+
       }
     }
     return retObj;
@@ -354,20 +361,20 @@ public class InventoryManagementImpl implements InventoryManagementAPI {
     try {
       DataDAOFactory daoFactry = new DataDAOFactory();
       SessionFactory sessionFactry = FactoryUtil.getSessionFactory();
-      
+
       txn = sessionFactry.getCurrentSession().beginTransaction();
       List<Item> items = daoFactry.getItemDAO().findAll();
       txn.rollback();
-      
+
       retObj.setReturnData(items);
-      
-    }catch(Throwable th) {
+
+    } catch (Throwable th) {
       retObj.setError(true);
       retObj.setException(th);
       try {
         txn.rollback();
       } catch (Exception e) {
-        
+
       }
     }
     return retObj;
@@ -380,20 +387,20 @@ public class InventoryManagementImpl implements InventoryManagementAPI {
     try {
       DataDAOFactory daoFactry = new DataDAOFactory();
       SessionFactory sessionFactry = FactoryUtil.getSessionFactory();
-      
+
       txn = sessionFactry.getCurrentSession().beginTransaction();
       Item item = daoFactry.getItemDAO().findByPrimaryKey(itemId);
       txn.rollback();
-      
+
       retObj.setReturnData(item);
-      
-    }catch(Throwable th) {
+
+    } catch (Throwable th) {
       retObj.setError(true);
       retObj.setException(th);
       try {
         txn.rollback();
       } catch (Exception e) {
-        
+
       }
     }
     return retObj;
@@ -406,27 +413,27 @@ public class InventoryManagementImpl implements InventoryManagementAPI {
     try {
       DataDAOFactory daoFactry = new DataDAOFactory();
       SessionFactory sessionFactry = FactoryUtil.getSessionFactory();
-      
+
       txn = sessionFactry.getCurrentSession().beginTransaction();
       List<Variant> items = daoFactry.getVariantDAO().findAll(itemId);
       txn.rollback();
-      
+
       List<Variant> itemClones = null;
-      if(items != null) {
+      if (items != null) {
         itemClones = new ArrayList<>();
-        for(Variant var : items) {
-          itemClones.add((Variant)var.clone());
+        for (Variant var : items) {
+          itemClones.add((Variant) var.clone());
         }
       }
       retObj.setReturnData(itemClones);
-      
-    }catch(Throwable th) {
+
+    } catch (Throwable th) {
       retObj.setError(true);
       retObj.setException(th);
       try {
         txn.rollback();
       } catch (Exception e) {
-        
+
       }
     }
     return retObj;
@@ -439,20 +446,20 @@ public class InventoryManagementImpl implements InventoryManagementAPI {
     try {
       DataDAOFactory daoFactry = new DataDAOFactory();
       SessionFactory sessionFactry = FactoryUtil.getSessionFactory();
-      
+
       txn = sessionFactry.getCurrentSession().beginTransaction();
       Variant variant = daoFactry.getVariantDAO().findByPrimaryKey(variantId);
       txn.rollback();
-      
-      retObj.setReturnData(variant == null ? null : (Variant)variant.clone());
-      
-    }catch(Throwable th) {
+
+      retObj.setReturnData(variant == null ? null : (Variant) variant.clone());
+
+    } catch (Throwable th) {
       retObj.setError(true);
       retObj.setException(th);
       try {
         txn.rollback();
       } catch (Exception e) {
-        
+
       }
     }
     return retObj;
